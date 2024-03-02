0b97f59b5f330168d4219839672318a01bd0f065
Utkarsh Singh <MSUTKARSH@OUTLOOK.COM>
2024-03-03 00:23:25 +0530

    init

---
+++ sum.js
@@ -0,0 +1,6 @@
+"use strict";
+function calSum(a, b) {
+    return a + b;
+}
+let x = calSum(2, 3);
+console.log(x);
---
+++ sum.ts
@@ -0,0 +1,6 @@
+function calSum(a:number, b:number): number{
+    return a + b;
+}
+
+let x = calSum(2, 3);
+console.log(x);
